61a38b437ad8a55e7383fa139e028feef4510098
Denis <hexarany@gmail.com>
2022-09-27 12:08:06 +0300

    React Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () =>{
+  return <h2> Hello This World</h2>
+}
+
+// const Field = () =>{
+//   const holder = 'Enter here'
+//   const styleField ={
+//       width: '300px',
+//       height: '15px'
+//   };
+//   return <input 
+//   placeholder= {holder} 
+//   type='text' 
+//   style = {styleField}/>
+// }
+
+class Field extends React.Component {
+  render () {
+    const holder = 'Enter here'
+  const styleField ={
+      width: '300px',
+      height: '15px'
+  };
+
+  return <input 
+          placeholder= {holder} 
+          type='text' 
+          style = {styleField}/>
+  }
+}
+
+function Btn() {
+  const text = 'Log In'
+  const logged = true;
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.StrictMode>
+          <Header />
+      </React.StrictMode>
+     <Field/>
+     <Btn/>
+     <logged/>
     </div>
   );
 }
