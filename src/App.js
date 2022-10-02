997fb5af13f7859e491614d68f4ec7b918a7530a
Denis <hexarany@gmail.com>
2022-10-03 01:29:01 +0300

    add event

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import './App.css';
-
+import { Component } from 'react';
 const Header = () =>{
   return <h2> Hello This World</h2>
 }
-
-// const Field = () =>{
-//   const holder = 'Enter here'
-//   const styleField ={
-//       width: '300px',
-//       height: '15px'
-//   };
-//   return <input 
-//   placeholder= {holder} 
-//   type='text' 
-//   style = {styleField}/>
-// }
-
 class Field extends React.Component {
   render () {
     const holder = 'Enter here'
@@ -24,7 +11,6 @@ class Field extends React.Component {
       width: '300px',
       height: '15px'
   };
-
   return <input 
           placeholder= {holder} 
           type='text' 
@@ -38,6 +24,48 @@ function Btn() {
   return <button>{logged ? 'Enter' : text}</button>
 }
 
+class WhoAmi extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        years: 27,
+        text: 'Увеличить Возраст',
+        position: ''
+      }
+  }
+  nextYear = () =>{
+    console.log('***');
+    this.setState(state => ({
+        years: state.years + 0.5
+      }))
+  }
+
+
+  commitInputChanges = (e) => {
+    this.setState({
+      position: e.target.value
+    })
+  }
+
+  render () {
+    const {name, surname, link} = this.props;
+    const {position, years} = this.state
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, 
+            age - {years},
+            position - {position}</h1>
+        <a href={link}>My Profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type='text' onChange={(e) => this.commitInputChanges()} />
+        </form>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
@@ -47,6 +75,8 @@ function App() {
      <Field/>
      <Btn/>
      <logged/>
+     <WhoAmi name='Denis' surname='Matievici' link='facebook.com'/>
+     <WhoAmi name='Ivan' surname='Ivanov' link='facebook.com'/>
     </div>
   );
 }
